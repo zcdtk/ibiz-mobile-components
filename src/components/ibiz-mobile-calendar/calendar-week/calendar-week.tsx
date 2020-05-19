d8e4143442e4d82ec7c52050e5f4c14695cb384f
zcdtk <zcdtk@github.com>
2020-05-19 11:16:01 +0800

    初始化

--- src/components/ibiz-mobile-calendar/calendar-week/calendar-week.tsx
+++ src/components/ibiz-mobile-calendar/calendar-week/calendar-week.tsx
@@ -1,60 +1,23 @@
-import { Component, Host, h, Prop } from '@stencil/core';
-
-import { defaults } from '../config';
+import { Component, Host, h, VNode } from '@stencil/core';
 
 @Component({
-  tag: 'calendar-week',
-  styleUrl: 'calendar-week.scss',
-  scoped: true,
+    tag: 'calendar-week',
+    styleUrl: 'calendar-week.scss',
+    scoped: true,
 })
 export class CanlendarWeek {
-  _weekArray: string[] = defaults.WEEKS_FORMAT;
-  _displayWeekArray: string[] = this._weekArray;
-  _weekStart = 0;
-  @Prop()
-  color: string = defaults.COLOR;
 
-  @Prop() weekArray:string[];
-
-  @Prop() weekStart:number;
-
-  constructor() { }
-
-  componentDidLoad() {
-    if (this.weekArray && this.weekArray.length === 7) {
-      this._weekArray = [...this.weekArray];
+    /**
+     * 绘制内容
+     *
+     * @returns {VNode}
+     * @memberof CanlendarWeek
+     */
+    public render(): VNode {
+        return (
+            <Host>
+                <div>这个是日历周组件</div>
+            </Host >
+        );
     }
-    if (this.weekStart === 0 || this.weekStart === 1) {
-      this._weekStart = this.weekStart;
-    }
-    this.adjustSort();
-  }
-
-  adjustSort(): void {
-    if (this._weekStart === 1) {
-      const cacheWeekArray = [...this._weekArray];
-      cacheWeekArray.push(cacheWeekArray.shift());
-      this._displayWeekArray = [...cacheWeekArray];
-    } else if (this._weekStart === 0) {
-      this._displayWeekArray = [...this._weekArray];
-    }
-  }
-
-  public render() {
-    return (
-      <Host>
-        <slot>
-          <ion-toolbar class={'week-toolbar ' + this.color} no-border-top>
-            <ul class={'week-title ' + this.color}>
-              {
-                this._displayWeekArray.map((w: any) => {
-                  return <li>{w}</li>
-                })
-              }
-            </ul>
-          </ion-toolbar>
-        </slot>
-      </Host >
-    );
-  }
 }
